3fe0faca02de8bc2ee6789a6851e2180465a6434
Seth Ringling <sringling@avvo.com>
2017-04-05 12:24:52 -0700

    Update core job

--- app/workers/store_builds.rb
+++ app/workers/store_builds.rb
@@ -1,3 +1,5 @@
+#require 'net/http'
+
 class StoreBuilds
   include Sidekiq::Worker
   include Sidetiq::Schedulable
@@ -10,6 +12,32 @@ class StoreBuilds
   end
 
   def perform
+    recent_builds = CircleCi::RecentBuilds.get
+    if recent_builds.success?
+      recent_builds.body.each do |build|
+        artifacts = CircleCi::Build.artifacts(build['username'], build['reponame'], build['build_num'])
+        if artifacts.success?
+          if artifacts.body.size > 0
+            artifacts.body.each do |artifact|
+              uri = URI.parse(artifact['url'])
+              uri.query = {token: CircleCi.config.token}.to_query
 
+              if uri.path.include?('circle-junit')
+                artifact_data = Net::HTTP.get(uri)
+                if artifact_data.present?
+                  # Write this to s3 if it is not there yet
+                end
+              end
+            end
+          else
+            warn "No artifacts found for #{build['username']}/#{build['reponame']} build ##{build['build_num']} - #{build['build_url']}"
+          end
+        else
+          raise 'Could not get artifact data'
+        end
+      end
+    else
+      raise 'Could not get recent build data'
+    end
   end
 end
---
+++ config/initializers/circleci.rb
@@ -0,0 +1,3 @@
+CircleCi.configure do |config|
+  config.token = ENV['CIRCLE_CI_TOKEN']
+end
