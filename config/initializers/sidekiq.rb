81eec6494824db568e9f7453c00ce83ddd4a3287
Seth Ringling <sringling@avvo.com>
2017-04-07 10:52:42 -0700

    config both server & client

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -10,6 +10,10 @@ Sidekiq.configure_client do |config|
   config.redis = sidekiq_config
 end
 
+Sidekiq.configure_server do |config|
+  config.redis = sidekiq_config
+end
+
 Redis.current = Sidekiq.redis {|c| c}
 
 if Rails.configuration.x.sidekiq_inline
